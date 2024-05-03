9ed6126a430dd61de63f6fccdf703e457fcc035e
ephysians <chuksobinna80@gmail.com>
2024-05-03 16:45:27 +0100

    update on project

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import img12 from "../../assets/img/img12.jpeg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { Link as Element } from "react-scroll";
 import imag1 from "../../assets/img/img1.png";
 import img3 from "../../assets/img/img3.png";
 import img4 from "../../assets/img/img4.jpeg";
 import img6 from "../../assets/img/img6.jpg";
+import pp from "../../assets/img/pp.png";
 import YoutubeEmbed from "./YoutubeEmbed";
 import { Card } from "antd";
 import Meta from "antd/es/card/Meta";
@@ -24,6 +27,11 @@ function App() {
   const videoId = "EdcgHfIYL9M";
   const videoId1 = "0jIVO6zsrcc";
   const videoId2 = "1l58O2UNQgE";
+  const emailButtonRef = useRef(null);
+  const handleClick = () => {
+    const email = "mailto:ruthinasconcept1@gmail.com";
+    window.location.href = email; // Redirect to email address
+  };
 
   return (
     <div>
@@ -34,12 +42,11 @@ function App() {
       />
 
       <nav className="h-16 flex justify-between items-center text-white fixed w-full z-20">
-        {/* Content always visible */}
         <Element
           to="home"
           smooth={true}
           duration={500}
-          className="ml-12 text-xl cursor-pointer text-base font-lato font-lato-normal"
+          className="ml-12cursor-pointer text-base lg:text-xl font-lato font-lato-normal"
         >
           RUTHINAS - CONCEPTZ
         </Element>
@@ -70,6 +77,14 @@ function App() {
             SERVICES
           </Element>
           <Element
+            to="portfolio"
+            smooth={true}
+            duration={500}
+            className="mr-8 cursor-pointer text-sm font-lato font-lato-normal"
+          >
+            PORTFOLIO
+          </Element>
+          <Element
             to="contact"
             smooth={true}
             duration={500}
@@ -81,26 +96,26 @@ function App() {
       </nav>
       <Element name="home">
         <section className="h-screen relative">
-          <div className="absolute inset-0 bg-black opacity-40"></div>
+          <div className="absolute inset-0 bg-black "></div>
           <div
-            className={`absolute inset-0 bg-cover bg-no-repeat flex justify-center items-center text-white mix-blend-overlay`}
+            className={`absolute inset-0 bg-cover bg-no-repeat flex justify-center items-center text-white `}
             style={{ backgroundImage: `url(${imag1})` }}
           >
-            <div className="text-center z-20 mix-blend-mode: normal">
-              <h1 className="text-6xl mb-4 text-red-800 font-lato font-lato-normal">
-                HI, I'M RUTHAINAS
+            <div className=" relative text-center bottom-24 z-20 mix-blend-mode: normal bg-slate-600  rounded p-2">
+              <h1 className="text-6xl mb-1 text-white font-lato font-lato-normal">
+                HI, I'M RUTHINAS
               </h1>
-              <p className="text-lg text-red-800 font-serif font-serif-normal">
-                Voice-over artist & Video presenter
+              <p className="text-xl text-white font-lato font-lato-normal">
+                VOICE OVER ARTIST & VIDEO PRESENTER
               </p>
             </div>
           </div>
         </section>
       </Element>
       <Element name="about">
-        <section className="h-auto shadow-2xl rounded-md flex m-12 flex-col px-64 bg-gray-200 justify-center items-center text-center text-1xl">
+        <section className="h-auto shadow-2xl rounded-md flex m-12 flex-col px-12 md:px-24 bg-gray-200 justify-center items-center text-center text-1xl">
           <h2 className="text-3xl mt-12 font-lato font-lato-normal ">
-            A LITTLE BIT ABOUT ME
+            A LITTLE BIT ABOUT ME\
           </h2>
           <img
             src={img3}
@@ -110,7 +125,7 @@ function App() {
           />
           <div className="">
             <p className=" mt-8 mb-12 font-lato font-lato-normal">
-              Hello! I'm <span className="text-blue-700">Ruthainas,</span> a
+              Hello! I'm <span className="text-blue-700">Ruthinas,</span> a
               talented Multi-faceted Performer. I love telling stories and
               captivating audiences with my dynamic voice and charming on-screen
               presence. With years of experience, I specialize in creating
@@ -130,12 +145,12 @@ function App() {
         </section>
       </Element>
       <Element name="services">
-        <section className="h-auto shadow-2xl rounded-md m-12 px-12 py-24 bg-gray-100 justify-center items-center text-center text-1xl">
+        <section className="h-auto shadow-2xl rounded-md m-12 px-4 lg:px-12 py-24 bg-gray-100  flex flex-col  justify-center items-center text-center text-1xl">
           <h2 className="text-3xl font-lato font-lato-normal pt-8 pb-8 ">
             WHAT I DO
           </h2>
-          <div className="flex justify-between border mx-64 gap-12">
-            <div class=" px-4 flex flex-col border rounded-md shadow-lg shadow-black/30 ">
+          <div className="flex w-full justify-between mx-64 gap-12 flex-col lg:flex-row">
+            <div class=" px-4 flex flex-col rounded-md shadow-lg shadow-black/30 ">
               <div class="flex flex-col relative top-4 items-center">
                 <div class="flex text-white items-center justify-center p-8">
                   <img
@@ -164,7 +179,7 @@ function App() {
                 <br />
               </p>
             </div>
-            <div class=" px-4 flex flex-col border rounded-md shadow-lg shadow-black/30 ">
+            <div class=" px-4 flex flex-col  rounded-md shadow-lg shadow-black/30 ">
               <div class="flex flex-col relative top-4 items-center">
                 <div class="flex text-white items-center justify-center p-8">
                   <img
@@ -192,7 +207,7 @@ function App() {
                 <br />
               </p>
             </div>
-            <div class=" px-4 flex flex-col border rounded-md shadow-lg shadow-black/30 ">
+            <div class=" px-4 flex flex-col  rounded-md shadow-lg shadow-black/30 ">
               <div class="flex flex-col relative top-4 items-center">
                 <div class="flex text-white items-center justify-center p-8">
                   <img
@@ -223,12 +238,12 @@ function App() {
           </div>
         </section>
       </Element>
-      <Element name="projects">
-        <section className="h-auto bg-gray-200 shadow-2xl rounded-md m-12 px-12 py-24 justify-center items-center text-center text-1xl">
+      <Element name="portfolio">
+        <section className="h-auto shadow-2xl rounded-md pl-52 pr-48 pb-52 bg-gray-100 flex flex-col justify-center items-center text-center text-1xl">
           <h2 className="text-3xl font-lato font-lato-normal pt-8 pb-8 ">
             PORTFOLIO
           </h2>
-          <div className="flex justify-between border mx-64 gap-8 ">
+          <div className="flex w-full flex-col lg:flex-row  justify-between mx-64 gap-12 items-center ">
             <Card
               hoverable
               style={{ width: 380 }}
@@ -244,31 +259,57 @@ function App() {
               style={{ width: 380 }}
               cover={<YoutubeEmbed videoId={videoId1} />}
             >
-              <Meta
-                title="Europe Street beat"
-                description="www.instagram.com"
-              />
+              <Meta title="Aquafina Water" description="www.aquafina.com" />
             </Card>
             <Card
               hoverable
               style={{ width: 380 }}
               cover={<YoutubeEmbed videoId={videoId2} />}
             >
-              <Meta
-                title="Europe Street beat"
-                description="www.instagram.com"
-              />
+              <Meta title="Real Estate" description="www.amestate.com" />
             </Card>
           </div>
         </section>
       </Element>
       <Element name="contact">
-      <section className="h-auto shadow-2xl rounded-md m-12 px-12 py-24 bg-gray-100 justify-center items-center text-center text-1xl">
-          <h2 className="text-3xl font-bold">Contact</h2>
+        <section className="h-auto shadow-2xl rounded-md m-12 px-12 py-24 bg-gray-100 justify-center items-center text-center text-1xl ">
+          <div className="grid place-items-center">
+            <img src={pp} alt="paper-plane" className="h-14 w-14" />
+            <h2 className="text-3xl font-lato font-lato-normal pt-8 pb-8 ">
+              GET IN TOUCH!
+            </h2>
+            <p className="font-lato font-lato-normal text-base">
+              DIscuss your needs. Feel free to send me an email to explore{" "}
+              <br /> how I can contribute to your vision.
+            </p>
+            <div className="mt-8 px-6 py-3 border-2 border-black hover:bg-blue-900 hover:border-none hover:text-white">
+              <button
+                id="email-button"
+                ref={emailButtonRef}
+                onClick={handleClick}
+              >
+                Say Hello
+              </button>
+            </div>
+          </div>
         </section>
       </Element>
-      <footer className="bg-gray-800 h-48 flex justify-center items-center text-white">
-        &copy; 2024 Brittany Chiang
+      <footer className="bg-gray-900 h-64 flex flex-col justify-center items-center text-white">
+        <div className="justify-center items-center mb-2 ">
+          <a
+            href="https://www.instagram.com/ruthinasconceptz?utm_source=qr"
+            id="instagram"
+          >
+            <i class="fa-solid fa-brands fa-instagram bg-neutral-700 p-4 m-1"></i>
+          </a>
+          <a href="https://wa.me/08182128516">
+            <i
+              class="fa-solid fa-brands fa-whatsapp bg-neutral-700 p-4 m-1"
+              target="_blank"
+            ></i>
+          </a>
+        </div>
+        &copy; 2024 Ruthinas Concepts
       </footer>
     </div>
   );
